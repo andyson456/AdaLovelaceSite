39755caceca91d8e53698bf39225b54b2c885ede
Andrew Larson <andyson456@gmail.com>
2019-11-17 14:10:54 -0800

    Important people model created

---
+++ AdaLovelaceSite/Models/ImportantPeopleModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdaLovelaceSite.Models
+{
+	public class ImportantPeopleModel
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string BirthDate { get; set; }
+		public string DeathDate { get; set; }
+		public int Children { get; set; }
+		public string RecognizedFor { get; set; }
+	}
+}
