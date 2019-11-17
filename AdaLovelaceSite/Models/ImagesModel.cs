bd888c95815c597366c8b65e02f0e4178f833f3a
Andrew Larson <andyson456@gmail.com>
2019-11-16 18:09:06 -0800

    Image added to Index page

---
+++ AdaLovelaceSite/Models/ImagesModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdaLovelaceSite.Models
+{
+	public class ImagesModel
+	{
+		public string Image1 { get { return "Images/ada-lovelace-20825279-1-402.jpg"; } }
+	}
+}
