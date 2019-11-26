07c9fd7bc3bb2d3d6372323a4f1c182bceb63fa8
Andrew Larson <andyson456@gmail.com>
2019-11-25 20:09:41 -0800

    Changed UI in QuizResults

--- AdaLovelaceSite/Models/User.cs
+++ AdaLovelaceSite/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,7 @@ namespace AdaLovelaceSite.Models
 		private List<Score> userScores = new List<Score>();
 
 		public Guid UserKey { get; set; }
+		[Required]
 		public string UserName { get; set; }
 		public int Score { get; set; }
 
